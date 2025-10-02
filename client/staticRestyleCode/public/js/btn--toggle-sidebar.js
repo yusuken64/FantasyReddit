f03562d6a00988b6edd812bfa49cafc282d05798
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2025-10-02 12:07:27 -0400

    further a11y improvements

--- client/staticRestyleCode/public/js/btn--toggle-sidebar.js
+++ client/staticRestyleCode/public/js/btn--toggle-sidebar.js
@@ -1,29 +1,37 @@
 const sidebarToggleButton = document.getElementById('btn--toggle-sidebar');
 const sidebar = document.getElementById('sidebar');
 
+// Debounce
 const animating = false;
 
 sidebarToggleButton.addEventListener('click', (event) => {
     if (!animating) {
+        animating = true;
+        
         const isSidebarToggled = sidebarToggleButton.getAttribute('aria-expanded');
     
         // User tampered with the aria-expanded value.
         if (isSidebarToggled != 'true' && isSidebarToggled != 'false') {
             throw new Error(`Invalid aria-expanded value provided: '${isSidebarToggled}'.`)
         }
-    
-        const ariaExpandedBoolNegated = isSidebarToggled == 'false' ? 'true' : 'false';
         
-        // Trigger close animation to occur
-    
+        const negatedIsSidebarToggled = isSidebarToggled == 'false' ? 'true' : 'false';
+        
+        if (negatedIsSidebarToggled) {
+            mobileSidebar.showPopover(); // popover, as I like this behavior compared to trapping focus.
+        }
+        
+        // Trigger close animation to occur (currently not implemented yet)
         sidebarToggleButton.setAttribute(
             'aria-expanded',
-            ariaExpandedBoolNegated
+            negatedIsSidebarToggled
         );
         
         // Wait for current animation to finish
         sidebarToggleButton.addEventListener('animationend', () => {
-            animating = true;
+            animating = false;
+            
+            mobileSidebar.hidePopover();
         });
     }
 });
