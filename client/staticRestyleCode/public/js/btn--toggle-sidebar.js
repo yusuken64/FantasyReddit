b701d0fe444858f0bb8a034f8b6025a38cfd9c38
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2025-10-05 16:31:12 -0400

    Fix sidebar animations

--- client/staticRestyleCode/public/js/btn--toggle-sidebar.js
+++ client/staticRestyleCode/public/js/btn--toggle-sidebar.js
@@ -4,22 +4,25 @@ const sidebar = document.getElementById('sidebar');
 // Debounce
 let isAnimating = false;
 
+// Updated sidebar toggle state for use in event handler
+let newIsSidebarToggled;
+
+// Handle toggle button click
 sidebarToggleButton.addEventListener('click', (event) => {
     if (!isAnimating) {
         isAnimating = true;
         
         const initialIsSidebarToggled = sidebarToggleButton.getAttribute('aria-expanded');
-    
+        
         // User tampered with the aria-expanded value.
         if (initialIsSidebarToggled != 'true' && initialIsSidebarToggled != 'false') {
             throw new Error(`Invalid aria-expanded value provided: '${initialIsSidebarToggled}'.`)
         }
         
         // firstIsSidebarToggled is a string, so we must use this logic to negate it,
-        // ... as Boolean(non_empty_string) results in true
-        const newIsSidebarToggled = initialIsSidebarToggled == 'false' ? 'true' : 'false';
+        // ... as Boolean(non_empty_string) results in `true`.
+        newIsSidebarToggled = initialIsSidebarToggled == 'false' ? 'true' : 'false';
         
-        // Will also trigger a close animation to occur (currently not implemented yet in the CSS)
         sidebarToggleButton.setAttribute(
             'aria-expanded',
             newIsSidebarToggled
@@ -29,15 +32,39 @@ sidebarToggleButton.addEventListener('click', (event) => {
             // Play closing animation
             sidebar.classList.add('closing');
         }
-        
-        // Wait for current animation to finish
-        sidebar.addEventListener('animationend', () => {
-            // If user toggled to close, cleanup class after close animation completes.
-            if (newIsSidebarToggled == 'false') {
-                sidebar.classList.remove('closing');
-            }
+        else {
+            // We want to manually control the popover instead of using the popovertarget attribute,
+            // ... since the animation will go out of sync given that the popover events aren't bound
+            // ... by the animation by default.
+            // Thus we handle shows and hides via JS ourselves, not with the free popovertarget attr for a button.
+            // We shouldn't use the beforetoggle event either, since we can only cancel a show state,
+            // ... not a hide state.
             
-            isAnimating = false;
-        });
+            sidebar.showPopover(); // Acts like `display: block | inline`
+            // Play opening animation
+            sidebar.classList.add('opening');
+        }
     }
+});
+
+// Handle animation finish
+sidebar.addEventListener('animationend', (event) => {
+    // Note to self for the future:
+    // We should define this event listener outside of the click event.
+    // For one, it would add a new event listener for each click, which is not the desired logic.
+    // Secondly, if we were to also define newIsSidebarToggled within the click handler,
+    // ... a 'animationend' closure will reference the newIsSidebarToggled definition from when the event listener
+    // ... was first defined (when the user first opens the sidebar).
+    // Multiple animationend event listeners would fight, and also
+    // ... a new newIsSidebarToggled variable would be created for each click.
+    
+    if (newIsSidebarToggled == 'false') {
+        sidebar.classList.remove('closing');
+        sidebar.hidePopover(); // Acts like `display: none`
+    }
+    else {
+        sidebar.classList.remove('opening');
+    }
+    
+    isAnimating = false;
 });
