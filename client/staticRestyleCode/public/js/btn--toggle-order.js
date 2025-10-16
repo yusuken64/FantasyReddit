7772afae50c5987aaaa044448e86fc6dbad61f7a
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2025-10-16 12:19:57 -0400

    Animation progress

--- client/staticRestyleCode/public/js/btn--toggle-order.js
+++ client/staticRestyleCode/public/js/btn--toggle-order.js
@@ -1,6 +1,41 @@
 const sortControlDivs = Array.from(document.querySelectorAll('#sort-fieldset .sort-control'));
 const enableSortRuleCheckboxInputs = Array.from(document.querySelectorAll('#sort-fieldset .control__input'));
 
+/*
+ * Add +180deg to the arrow's rotation.
+ * If the current rotation is 360deg, reset to 0.
+ * Saves memory (given the reset) and is the best way to achieve a
+ * counter-clockwise rotation animation like this.
+ */
+function rotateSortIndicator(sortIndicatorElement) {
+    debugger;
+    const currentStyleObject = getComputedStyle(sortIndicatorElement);
+    const currentRotationValueString = currentStyleObject.getPropertyValue("rotate");
+    const currentRotationDeg = Number(currentRotationValueString.slice(0, -3)); // remove 'deg' from the end
+    
+    // Don't use up more memory than we need.
+    if (currentRotationDeg == 360) {
+        currentRotationDeg = 0;
+    }
+    
+    const keyFrame = new KeyframeEffect(
+        sortIndicatorElement,
+        // Properties
+        [
+            { rotate: `${currentRotationDeg + 180}deg` },
+        ],
+        // Keyframe options
+        {
+            duration: .2 * 1000, // .2 seconds
+            easing: "ease",
+            composite: "add",
+            fill: "forwards",
+        },
+    )
+    const animationTo = new Animation(keyFrame);
+    animationTo.play();
+}
+
 // Hairy state management. A framework might make this a lot better.
 
 // Controls default to descending order until the user toggles the order.
@@ -34,6 +69,7 @@ sortControlDivs.forEach(sortControlDiv => {
         
         a11yCurrentOrderTextSpan.textContent = negatedOrderString;
         
+        rotateSortIndicator(currentSortOrderIndicator);
         // currentSortOrderIndicator.textContent = negatedOrderIconText;
         
         // Send a web request and update the URL to include query parameters (in case the user shares their query to other users via URL)
